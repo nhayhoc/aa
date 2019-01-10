5d8c4c63e5b47eaa3d93051226947205c001c261
admin <admin@admins-MacBook-Pro.local>
2019-01-10 23:41:54 +0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+const fetch = require("node-fetch");
+const Page = ({ stars, isServer }) => (
+  <div>
+    <div>
+      Next stars: {stars} <br /> isServer: {isServer.toString()}
+    </div>
+    <br />
+    <Link href="/page2">
+      <a>Navigate</a>
+    </Link>
+  </div>
+);
+
+Page.getInitialProps = async ({ req }) => {
+  console.log({ req });
+  const res = await fetch("https://api.github.com/repos/zeit/next.js");
+  const json = await res.json();
+  return { stars: json.stargazers_count, isServer: !!req };
+};
+
+export default Page;
